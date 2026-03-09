1e7b2c60a6c6d421b24804f7904176998df2d94e
Puppet <122136792+Thepuppetqueen57@users.noreply.github.com>
2026-03-09 00:44:14 -0700

    Added code for replacing functions to the example mod
    For now you can only replace Player functions but dw I'll probably procrastinate fixing that for a while :)

--- example-mod/main.cs
+++ example-mod/main.cs
@@ -18,5 +18,29 @@ public class Mod
         {
             ((Button)node).Text = "Did you know that the game is modded?";
         });
+
+        // These functions are both on the same node so we can register them in the same callback
+        ModLoader.RegisterNodeCallback("Player", (node) => {
+            var player = (Player)node;
+            var originalDie = player.DieAction;
+            // Die takes no arguments so put nothing in the parentheses
+            // If you want to know if a function has arguments go into the source of the game
+            // Go to the script with the function and just check if it has any parameters
+            player.DieAction = () => {
+                GD.Print("Before death!");
+                originalDie?.Invoke();
+                GD.Print("After Death!");
+            };
+
+            var originalIsOnGround = player.IsOnGroundAction;
+            // IsOnGround takes a RigidBody2D as an argument so put (rb) in the parentheses
+            player.IsOnGroundAction = (rb) => {
+                var result = originalIsOnGround.Invoke(rb);
+                if (result == true) {
+                    GD.Print("Player is on the ground!");
+                }
+                return result;
+            };
+        });
     }
 }
